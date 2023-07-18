97c1dc1e14e8a6dd45fe6864fe7b57d02c723c61
rak Random <fellipeleo100@gmail.com>
2023-07-17 21:13:32 -0300

    Runneer V2.5

---
+++ README.md
@@ -0,0 +1,31 @@
+# Runneer
+
+Objetivo:
+Treinamento do módulo pygame, com a criação de um jogo do tipo endless runnerchamado "Runneer", usando como referência e auxílio o vídeo "The ultimate introduction to Pygame"¹
+
+Feito por: Fellipe Leonardo Peixoto Cunha²
+
+Com auxílio de: Clear Code³⁴
+
+Data de inicio: 14/07/2023
+Data de lançamento: 15/07/2023
+
+Referências:
+
+1. https://www.youtube.com/watch?v=AY9MnQ4x3zk
+2. https://github.com/rakRandom
+3. https://www.youtube.com/@ClearCode
+4. https://github.com/clear-code-projects
+
+Atualização V2 (16/07/2023):
+
+- Novo sistema de colisões usando mask
+- Audio e música
+- Menu de opções
+- Remoção de bugs
+
+Atualização V2.5 (17/07/2023):
+
+- Frame-Independent e a opção de mudar o FPS limite no 'settings.py'
+- Remoção do bug nos links de abrir multiplas janelas
+- Novo icone
---
+++ data/settings.py
@@ -0,0 +1,52 @@
+"""Settings for the "runneer.py" file"""
+
+# ====================== Importations =====================
+try:
+    import pygame
+
+    from time import time
+    from json import loads, dumps, load, dump
+    from webbrowser import open as op
+    from random import randint
+    from sys import exit
+except: raise ImportError
+
+with open("data/config.json", "r") as configs:
+    data = loads(configs.read())
+
+
+# ======================= Constants =======================
+GAME_NAME = data["GAME_NAME"]
+GAME_ICON = data["GAME_ICON"]
+ANIMATION_SPEED = data["ANIMATION_SPEED"]
+BACKGROUND_SPEED = data["BACKGROUND_SPEED"]
+
+
+# ======================== screen =========================
+SCREEN_WIDTH = data["SCREEN_WIDTH"]
+SCREEN_HEIGHT = data["SCREEN_HEIGHT"]
+
+
+# ========================= clock =========================
+FRAMES_PER_SECOND = data["FRAMES_PER_SECOND"]
+
+
+# ========================= enemy =========================
+SPAWN_RATE = data["SPAWN_RATE"]
+EASY_SPEED = data["EASY_SPEED"]
+MEDIUM_SPEED = data["MEDIUM_SPEED"]
+HARD_SPEED = data["HARD_SPEED"]
+FLY_RARITY = data["FLY_RARITY"]
+SNAIL_ANIMATION_SPEED = data["SNAIL_ANIMATION_SPEED"]
+FLY_ANIMATION_SPEED = data["FLY_ANIMATION_SPEED"]
+
+
+# ========================= player ========================
+JUMP_FORCE = data["JUMP_FORCE"]
+GRAVITY_ACCELERATION = data["GRAVITY_ACCELERATION"]
+
+
+
+
+
+
